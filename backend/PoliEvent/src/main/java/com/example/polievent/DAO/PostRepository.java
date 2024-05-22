5bccc8c12ccdcfc96acb7fca66514607dfe2e022
Dominik Klimczak <63956904+Olopa1@users.noreply.github.com>
2024-05-22 11:44:02 +0200

    Revert "Backend dla klas: Advertiser, Event, Post"

--- backend/PoliEvent/src/main/java/com/example/polievent/DAO/PostRepository.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-    //TODO...
-}
