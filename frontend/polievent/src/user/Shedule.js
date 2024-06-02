4e027f68811362643f1fa30251ae5df83f73d773
Dominik <245842@.edu.p.lodz.pl>
2024-06-02 23:03:09 +0200

    Dodanie planu możliwości dodawania usuwanie oraz edytowania po stronie klienta

--- frontend/polievent/src/user/Shedule.js
+++ frontend/polievent/src/user/Shedule.js
@@ -5,11 +5,39 @@ import Post from './PostonMainPage';
 import Menu from './Menu'
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import GridPosts from './GridPosts';
+import 'devextreme/dist/css/dx.light.css'; 
+import { Scheduler,View } from 'devextreme-react/cjs/scheduler';
+
+    const initialData = [];
 
 export const Shedule = function(){
+
+    const [classes,setClasses] = useState([]);
+    const onClassAdded = (e)=>{
+        setClasses([...classes, e.initialData]);
+    };
+    const onClassDeleted = (e)=>{
+        const filteredClasses = classes.filter(currentClass=>currentClass.id !== e.initialData.id);
+        setClasses(filteredClasses);
+    }
+
+
     return(
         <>
-            
+            <Scheduler 
+            dataSource={initialData}
+            defaultCurrentView='workWeek' 
+            allDayPanelMode='hidden'
+            showCurrentTimeIndicaton={true}
+            onAppointmentAdded={onClassAdded}
+            height={600}
+            >
+                <View
+                type="workWeek"
+                startDayHour={8}
+                endDayHour={21}
+                />
+            </Scheduler> 
         </>
     );
 }
