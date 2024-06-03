cb02a2e62c2a52c847785936312ac140eb6e03f8
igorgardzielewski <igorgardzielewski3@gmail.com>
2024-06-04 01:26:15 +0200

    Dodanie pola godziny

--- backend/PoliEvent/src/main/java/com/example/polievent/testData/postConfig.java
+++ backend/PoliEvent/src/main/java/com/example/polievent/testData/postConfig.java
@@ -15,8 +15,8 @@ public class postConfig {
     @Bean
     CommandLineRunner clr(PostRepository repository){
         return args -> {
-            Post first = new Post(1L,"politechnika na foodtruck","jest fajnie","Politechniczna 8","WPRE",0,0,50,LocalDate.now(),LocalDate.of(2024, Month.MAY,10), LocalTime.now());
-            Post sec = new Post(2L,"statki","ogieeeen","Marunska 8","qpres",0,0,100,LocalDate.now(),LocalDate.of(2024, Month.MAY,12),LocalTime.now());
+            Post first = new Post(1L,"politechnika na foodtruck","jest fajnie","Politechniczna 8","WPRE",0,0,50,LocalDate.now(),LocalDate.of(2024, Month.MAY,10), LocalTime.of(12,30));
+            Post sec = new Post(2L,"statki","ogieeeen","Marunska 8","qpres",0,0,100,LocalDate.now(),LocalDate.of(2024, Month.MAY,12),LocalTime.of(12,30));
             repository.saveAll(List.of(first,sec));
         };
     }
