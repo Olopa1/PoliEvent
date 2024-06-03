eb4e71d87a6248f055dc1498c3f0b9223b66beb6
igorgardzielewski <igorgardzielewski3@gmail.com>
2024-06-04 01:03:06 +0200

    Dodanie czasu rozpoczecia eventu

--- backend/PoliEvent/src/main/java/com/example/polievent/testData/postConfig.java
+++ backend/PoliEvent/src/main/java/com/example/polievent/testData/postConfig.java
@@ -4,6 +4,8 @@ import com.example.polievent.DAO.PostRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalTime;
 import java.util.Date;
 import java.time.LocalDate;
 import java.time.Month;
@@ -13,8 +15,8 @@ public class postConfig {
     @Bean
     CommandLineRunner clr(PostRepository repository){
         return args -> {
-            Post first = new Post(1L,"politechnika na foodtruck","jest fajnie","Politechniczna 8","WPRE",0,0,50,LocalDate.of(2024, Month.MAY,31),LocalDate.of(2024, Month.MAY,10));
-            Post sec = new Post(2L,"statki","ogieeeen","Marunska 8","qpres",0,0,100,LocalDate.of(2024, Month.MAY,31),LocalDate.of(2024, Month.MAY,12));
+            Post first = new Post(1L,"politechnika na foodtruck","jest fajnie","Politechniczna 8","WPRE",0,0,50,LocalDate.now(),LocalDate.of(2024, Month.MAY,10), LocalTime.now());
+            Post sec = new Post(2L,"statki","ogieeeen","Marunska 8","qpres",0,0,100,LocalDate.now(),LocalDate.of(2024, Month.MAY,12),LocalTime.now());
             repository.saveAll(List.of(first,sec));
         };
     }
