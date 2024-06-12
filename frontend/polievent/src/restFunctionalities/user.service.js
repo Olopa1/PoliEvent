1916bac0e0d503cd9cb71251975f4f668c932b5e
Dominik Klimczak <63956904+Olopa1@users.noreply.github.com>
2024-06-12 12:54:13 +0200

    Revert "Revert "Dodanie planu zajęć""

--- frontend/polievent/src/restFunctionalities/user.service.js
+++ frontend/polievent/src/restFunctionalities/user.service.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { ApiKey } from "devextreme-react/cjs/map";
 const APIR_URL = "http://localhost:8080";
 
 class UserService{
@@ -13,10 +14,17 @@ class UserService{
       });
   }
   async saveShedule(shedules){
-    return await axios.post(APIR_URL + '/saveShedule',{shedules});
+    return await axios.post(APIR_URL + '/saveShedule',shedules,{
+      headers:{'Content-Type' : 'application/json'}
+    });
   }
+
   async deleteShedulesWithId(id){
-    return await axios.delete(APIR_URL + '/deleteShedule',{id});
+    return await axios.delete(APIR_URL + '/deleteShedule',{params: {id}});
+  }
+
+  async getInitalDataForShedule(id){
+    return await axios.get(APIR_URL + '/readSheduleWithId',{params:{id: id}});
   }
 }
 
